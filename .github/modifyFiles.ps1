6826b8e3564e6f04826a507996d629431ecd8deb
AlvaroMartinezGuija <amartinez@tecon.es>
2025-07-21 11:53:12 +0200

    fv<xc

--- .github/modifyFiles.ps1
+++ .github/modifyFiles.ps1
@@ -1,24 +1,24 @@
-# Import the necessary module
-#Import-Module -Name 'PSScriptRoot'
+# Import the necessary modules
+Import-Module -Name Json
 
 $defaultUrl = 'https://www.tecon.es/'
 $defaultLogo = './Logo/Tecon.png'
 $fieldsToCheck = @('privacyStatement', 'EULA', 'help', 'url')
 
 # Read the JSON file
-$jsonContent = Get-Content -Path 'app.json' -Raw -Encoding utf8 | ConvertFrom-Json
+$data = Get-Content -Path './app.json' -Raw | ConvertFrom-Json
 
 foreach ($field in $fieldsToCheck) {
-    if (-not $jsonContent.$field) {
-        $jsonContent | Add-Member -MemberType NoteProperty -Name $field -Value $defaultUrl
+    if (-not $data.$field) {
+        $data | Add-Member -MemberType NoteProperty -Name $field -Value $defaultUrl
     }
 }
 
-if (-not $jsonContent.logo) {
-    $jsonContent | Add-Member -MemberType NoteProperty -Name logo -Value $defaultLogo
+if (-not $data.logo) {
+    $data | Add-Member -MemberType NoteProperty -Name logo -Value $defaultLogo
 }
 
-$jsonContent.version = "2.$((Get-Date).ToString('yyyyMMdd')).0.0"
+$data.version = "2.$((Get-Date).ToString('yyyyMMdd')).0.0"
 
-# Write the updated JSON back to the file
-$jsonContent | ConvertTo-Json -Depth 4 | Set-Content -Path 'app.json' -Encoding utf8
+# Write the updated data back to the JSON file
+$data | ConvertTo-Json -Depth 10 | Set-Content -Path 'app.json' -Encoding utf8
