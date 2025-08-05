0849b2a88614cfc5ba13bdf909edcf43e0c18a37
AlvaroMartinezGuija <amartinez@tecon.es>
2025-08-05 12:22:07 +0200

    añadir archivos metadata y conexion akv con ghactions secrets

--- .github/scripts/ghtokenrotation.ps1
+++ .github/scripts/ghtokenrotation.ps1
@@ -2,52 +2,56 @@ param (
         [String] $keyvaultname,
         [String] $matchPattern,
         [String] $action,
-        [String] $metadataPath
+        [String] $metadataPath,
+        [String] $organization
     )
-#     $tokenData = az keyvault secret list --vault-name $keyvaultname  | ConvertFrom-Json
-#     $data= Get-Content $metadataPath | ConvertFrom-Json
-#     $tokens = $tokenData.name -match $matchPattern
+    $tokenData = az keyvault secret list --vault-name $keyvaultname  | ConvertFrom-Json
+    $data= Get-Content $metadataPath | ConvertFrom-Json
+    $tokens = $tokenData.name -match $matchPattern
 
-#     $expiring = [datetime]::Parse($data.expires).ToUniversalTime()
-#     $today = Get-Date
-#     $diff = ($expiring - $today).Days
+    $expiring = [datetime]::Parse($data.expires).ToUniversalTime()
+    $today = Get-Date
+    $diff = ($expiring - $today).Days
 
-#     if ($tokens -cnotmatch 'False'){
-#         if ($diff -le 7) {
-#             Write-Host "Hay que rotar (faltan $diff días)"
-#             $nextToken = $tokenData | Where-Object {$_.name -match $matchPattern}| Sort-Object { [datetime]::Parse($_.attributes.expires)} | Select-Object -First 1
-#             $newexpiring = [datetime]::Parse($nextToken.attributes.expires).ToUniversalTime()
-#             $tokenName = $nextToken.Name
-#             if ($newexpiring -gt $expiring){
-#                 Write-Host $newexpiring
-#                 $data.expires = $newexpiring.ToString("yyyy-MM-ddTHH:mm:ssZ")
-#                 $data.token_name = $tokenName
-#                 $data | ConvertTo-Json -Depth 2 | Set-Content $metadataPath -Encoding UTF8
-#                 Write-Host "actualizado el token a $tokenName (expira el $($newexpiring.ToString("dd/MM/yyyy HH:mm:ss")))"
-#                 $value = (az keyvault secret show --name $nextToken.name --vault $keyvaultname | ConvertFrom-Json).value
-#             }else {
-#                 Write-Host "La fecha a la que se va a cambiar es anterior o igual a la que hay actualmente por lo que se eliminará el token más antiguo"
-#             }
-#             az keyvault secret delete --vault-name $keyvaultname --name $tokenName
-#         } else {
-#             Write-Host "Por el momento no hay que rotar (faltan $diff días)"
-#         }           
-#     } else{
-#         Write-Host "No quedan tokens en el pool tienes que crear mas"
-#     }
+    if ($tokens.Count -le 1){
+        if ($diff -le 7) {
+            Write-Host "Hay que rotar (faltan $diff días)"
+            $nextToken = $tokenData | Where-Object {$_.name -match $matchPattern}| Sort-Object { [datetime]::Parse($_.attributes.expires)} | Select-Object -First 1
+            $newexpiring = [datetime]::Parse($nextToken.attributes.expires).ToUniversalTime()
+            $tokenName = $nextToken.Name
+            if ($newexpiring -gt $expiring){
+                Write-Host $newexpiring
+                $data.expires = $newexpiring.ToString("yyyy-MM-ddTHH:mm:ssZ")
+                $data.token_name = $tokenName
+                $data | ConvertTo-Json -Depth 2 | Set-Content $metadataPath -Encoding UTF8
+                Write-Host "actualizado el token a $tokenName (expira el $($newexpiring.ToString("dd/MM/yyyy HH:mm:ss")))"
+                $value = (az keyvault secret show --name $nextToken.name --vault $keyvaultname | ConvertFrom-Json).value
+                  az keyvault secret set --name 'testing' --value $value --expires $data.expires --vault-name $keyvaultname
+            }else {
+                Write-Host "La fecha a la que se va a cambiar es anterior o igual a la que hay actualmente por lo que se eliminará el token más antiguo"
+            }
+            az keyvault secret delete --vault-name $keyvaultname --name $tokenName
+        } else {
+            Write-Host "Por el momento no hay que rotar (faltan $diff días)"
+        }           
+    } else{
+        Write-Host "No quedan tokens en el pool tienes que crear mas"
+    }
 
-$value='testing'
+# $value='testing'
 switch ($action) {
     'Workflow' { 
         #$value
         $matchPattern
-        #gh secret set -o $organization GHTOKENWORKFLOW
+        #gh secret set -o $organization ghTokenWorkflow -b $value
      }
      'StorageAccountDelivery'{
         $matchPattern
+        #gh secret set StorageContext -b $value
      }
      'ghPackagesDeliver'{
         $matchPattern
+        #gh secret set -o $organization GitHubPackageContext -b $value
      }
      'environment'{
           $environments = ConvertFrom-Json $env:ENVJSON
@@ -56,12 +60,8 @@ switch ($action) {
                 $obj = $env.$key
                 $envName= $obj.EnvironmentName
                 $secretName = (gh secret list -e $envName --json name | ConvertFrom-Json).name
-                Write-Host '------------nombre del secreto:' $secretName
                 if ($envName -like 'test'){
                   gh secret set $secretName -e $envName -b $value
-                  Write-Host '------------nombre del secreto:' $secretName
-                  Write-Host '------------nombre del entorno:' $envName
-                  Write-Host '-------------------------valor:' $value
                 }
               }
             }
