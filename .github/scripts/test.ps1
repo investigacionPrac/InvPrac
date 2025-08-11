f4f6aec64ceb1b0cbededdcf61b9a4aaeb837601
AlvaroMartinezGuija <amartinez@tecon.es>
2025-08-11 09:36:39 +0200

    probar a llamar a un yaml dentro de un ps1 desde otro yaml

--- .github/scripts/test.ps1
+++ .github/scripts/test.ps1
@@ -1,3 +1,6 @@
+$environments = (gh api repos/$env:OWNER/$appRepo/environments) | ConvertFrom-Json
+$names = $environments.environments.Name
+
 foreach ($envName in $names){
-        gh workflow run "test.yaml" --ref main --field entorno=$envName
+        gh workflow run "test2.yaml" --ref main --field entorno=$envName
     }
