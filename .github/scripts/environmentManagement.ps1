99bd9546a4b01daef64c41ce7fc5cf2653387ae1
AlvaroMartinezGuija <amartinez@tecon.es>
2025-08-14 12:02:20 +0200

    crear entornos para cada cliente usando Get-BcPublishedApps

--- .github/scripts/environmentManagement.ps1
+++ .github/scripts/environmentManagement.ps1
@@ -4,29 +4,46 @@ param (
     [String] $client
 )
 
-$data = Get-Content '.\.github\metadata\clientes-de-testing.json' | ConvertFrom-Json
+$authContext = New-BcAuthContext -clientID $env:CLIENTID -clientSecret $env:CLIENTSECRET -tenantID $env:TENANTID
 
 $appRepo = Split-Path $repoPath -Leaf
 
-$environments = (gh api repos/$env:OWNER/$appRepo/environments) | ConvertFrom-Json
-$names = $environments.environments.Name
-Write-Host "Entornos en el repo: $names"
-if ($action -eq 'crear') { 
-    foreach ($client in $data.PSObject.Properties.Name) {
+
+$environmentsBC = Get-BcEnvironments -bcAuthContext $authContext
+$environmentsGH = (gh api repos/$env:OWNER/$appRepo/environments) | ConvertFrom-Json
+$environmentsGHNames = $environmentsGH.environments.Name
+$environmentsBCNames = @()
+$clientes = @()
+if ($action -eq 'crear') {
+    for ($i = 0; $i -lt $envirnomentsBC.Length; $i++) {
+        $environmentsBCNames += $environmentsBC[$i].Name + ""
+    }
+    foreach ($client in $environmentsBCNames) {
+        $appNames = @()
+        $clientApps = @()
         Write-Host "Evaluando al cliente $client"
-        if ($data.$client.Contains($appRepo)) {
-            $clientes += $client + " "
-            if ($names.Contains($client)) {
-                Write-Warning "El entorno $client ya existe por lo que no se creará ningún entorno con ese nombre"
+        $clientApps = Get-BcPublishedApps -bcAuthContext $authContext -environment $client
+        
+        for ($i = 0; $i -lt $clientApps.Length; $i++) {
+            $appNames += $clientApps[$i].Name + ""
+        }
+        foreach ($app in $appNames) {
+            if ($app -eq $appRepo) {
+                $clientes += $client + " "
+                if ($environmentsGHNames.Contains($client)) {
+                    Write-Warning "El entorno $client ya existe por lo que no se creará ningún entorno con ese nombre"
+                }
+                else {
+                    gh api --method PUT -H "Accept: application/vnd.github+json" repos/$env:OWNER/$appRepo/environments/$client
+                    Write-Host "Entorno $client creado correctamente"
+                }
             }
             else {
-                gh api --method PUT -H "Accept: application/vnd.github+json" repos/$env:OWNER/$appRepo/environments/$client
-                Write-Host "Entorno $client creado correctamente"
+                Write-Warning "La aplicación $app no está publicada en el entorno $client, por lo que no se creará ningún entorno con ese nombre"
             }
-            
+
         }
     }
-
 }
 elseif ($action -eq 'actualizarPTE') {
     $settings = Get-Content '.github\AL-Go-Settings.json' -Raw | ConvertFrom-Json
@@ -37,5 +54,3 @@ elseif ($action -eq 'actualizarPTE') {
     $settings | Add-Member -NotePropertyName "DeployTo$client" -NotePropertyValue $PTE
     $settings | ConvertTo-Json -Depth 10 | Set-Content '.github\AL-Go-Settings.json'
 }
-
-Write-Host "Los clientes que tienen la app buscada son: $clientes"
